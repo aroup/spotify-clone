6cdccb1f2e3502d675c19b790079ee53129f9cf4
Aroup Goldar <aroupdhruba@gmail.com>
2016-09-05 22:07:41 +0600

    Fifth Change

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,29 @@
-const webpack = require('webpack');
-const path = require('path');
-
-const PATHS = {
-  app: './src/index.js',
-  html: './src/index.html',
-  dist: path.join(__dirname, 'dist')
-};
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  entry: {
-    javascript: PATHS.app,
-    html: PATHS.html
+  entry: [
+    './src/index'
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style!css!sass' },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
   },
   output: {
-    path: PATHS.dist,
+    path: path.join(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: PATHS.dist,
-    hot : true
+    contentBase: './dist',
+    hot: true
   },
-  eslint: {
-    emitWarning: true
-  },
-  module: {
-    preLoaders: [
-      {
-        test: /\.(js|jsx)?$/,
-        loaders: ["eslint-loader"],
-        exclude: /node_modules/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]"
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loaders: ["react-hot", "babel-loader"]
-      }
-    ]
-  },
-  resolve: {
-   extensions: ['', '.js', '.jsx']
- },
- plugins: [
+  plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
